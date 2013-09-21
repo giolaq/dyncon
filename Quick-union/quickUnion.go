b042d4c4db6e823342ade392b7970a12a33e4d3d
joaobiriba <glaquidara@gmail.com>
2013-09-21 23:04:03 +0200

    A taste of gofmt

--- Quick-union/quickUnion.go
+++ Quick-union/quickUnion.go
@@ -1,39 +1,40 @@
 package main
+
 import "fmt"
 
-func quickUnion(N int)  []int{
-	id := make([]int, N)  
+func quickUnion(N int) []int {
+	id := make([]int, N)
 	for i := range id {
-		    id[i] = i;
-	    }
-	return id  
+		id[i] = i
+	}
+	return id
 }
 
 func root(id []int, i int) int {
 	for i != id[i] {
-		i = id[i] 
+		i = id[i]
 	}
 	return i
 }
 
 func connected(id []int, p, q int) bool {
-	return root(id,p) == root(id, q)
+	return root(id, p) == root(id, q)
 }
 
 func union(id []int, p, q int) {
 	i := root(id, p)
 	j := root(id, q)
 	id[i] = j
-	
+
 }
 
 func main() {
 	id := quickUnion(10)
-    union(id,5,0)
-	union(id,5,6)
-	union(id,1,2)
-	union(id,8,3)
-	union(id,4,9)
-	fmt.Printf ("connected 1,2 ? = %v \n", connected(id,1,2) )
-	fmt.Printf ("connected 1,7 ? = %v \n", connected(id,1,7) )
+	union(id, 5, 0)
+	union(id, 5, 6)
+	union(id, 1, 2)
+	union(id, 8, 3)
+	union(id, 4, 9)
+	fmt.Printf("connected 1,2 ? = %v \n", connected(id, 1, 2))
+	fmt.Printf("connected 1,7 ? = %v \n", connected(id, 1, 7))
 }
--- Quick-union/quickUnionWeighted.go
+++ Quick-union/quickUnionWeighted.go
@@ -1,48 +1,49 @@
 package main
+
 import "fmt"
 
-func quickUnionWeighted(N int)  ([]int, []int){
-	id := make([]int, N) 
-	sz := make([]int, N)  
-	 
+func quickUnionWeighted(N int) ([]int, []int) {
+	id := make([]int, N)
+	sz := make([]int, N)
+
 	for i := range id {
-		    id[i] = i;
-			sz[i] = 1;
-	    }
+		id[i] = i
+		sz[i] = 1
+	}
 	return id, sz
 }
 
 func root(id []int, i int) int {
 	for i != id[i] {
-		i = id[i] 
+		i = id[i]
 	}
 	return i
 }
 
 func connected(id []int, p, q int) bool {
-	return root(id,p) == root(id, q)
+	return root(id, p) == root(id, q)
 }
 
 func union(id, sz []int, p, q int) {
 	i := root(id, p)
 	j := root(id, q)
-	if sz[i] < sz [j] {
+	if sz[i] < sz[j] {
 		id[i] = j
 		sz[j] = sz[j] + sz[i]
 	} else {
 		id[j] = i
 		sz[i] = sz[i] + sz[j]
 	}
-	
+
 }
 
 func main() {
 	id, sz := quickUnionWeighted(10)
-    union(id,sz,5,0)
-	union(id,sz,5,6)
-	union(id,sz,1,2)
-	union(id,sz,8,3)
-	union(id,sz,4,9)
-	fmt.Printf ("connected 1,2 ? = %v \n", connected(id,1,2) )
-	fmt.Printf ("connected 1,7 ? = %v \n", connected(id,1,7) )
+	union(id, sz, 5, 0)
+	union(id, sz, 5, 6)
+	union(id, sz, 1, 2)
+	union(id, sz, 8, 3)
+	union(id, sz, 4, 9)
+	fmt.Printf("connected 1,2 ? = %v \n", connected(id, 1, 2))
+	fmt.Printf("connected 1,7 ? = %v \n", connected(id, 1, 7))
 }
