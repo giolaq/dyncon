b995c3c7629f976f4894b21e45cf5d732fd4cb9a
joaobiriba <glaquidara@gmail.com>
2013-10-02 23:25:30 +0200

    Test procedures for Quick-Find and Quick-Union

--- quickFind_test.go
+++ quickFind_test.go
@@ -7,29 +7,57 @@ import (
 	"testing"
 )
 
-func TestSqrt(t *testing.T) {
-	file, err := os.Open("mediumUF.txt")
+type union func(int, int)
+
+func TestQuickFind(t *testing.T) {
+	file, err := os.Open("tinyUF.txt")
 	if err != nil {
 		t.Errorf("Error opening File")
 	}
 	defer file.Close()
 
-
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)
 	scanner.Scan()
 	size, _ := strconv.Atoi(scanner.Text())
-	
+
 	qf := initQuickFind(size)
 
 	for scanner.Scan() {
 		x, _ := strconv.Atoi(scanner.Text())
 		scanner.Scan()
 		y, _ := strconv.Atoi(scanner.Text())
-		qf.Union(x, y)		
+		qf.Union(x, y)
 	}
-	
+
 	if conn := qf.Connected(6, 1); conn != true {
+		t.Errorf("Error")
+	}
+
+}
+
+func TestQuickUnion(t *testing.T) {
+	file, err := os.Open("tinyUF.txt")
+	if err != nil {
+		t.Errorf("Error opening File")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	size, _ := strconv.Atoi(scanner.Text())
+
+	uf := initQuickUnionUF(size)
+
+	for scanner.Scan() {
+		x, _ := strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		y, _ := strconv.Atoi(scanner.Text())
+		uf.union(x, y)
+	}
+
+	if conn := uf.connected(6, 1); conn != true {
 		t.Errorf("Error")
 	}
 }
